db46b4d97b5b17087a86aae79d07bc20c7dc5785
Lee Doughty <32392044+leedoughty@users.noreply.github.com>
2018-07-29 18:09:10 +0100

    Updating Virtual Freeplay title and background image

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          <h1 className="title">VIRTUAL FREEPLAY</h1>
+        </header>
+        <img className="bg-image" src={require("./images/bg.jpg")}/>
+      </div>
+    );
+  }
+}
+
+export default App;
